879356557c488d4df8363a4e5318d3c71859836e
Matrixoid <mr.art.pro@mail.ru>
2021-06-08 13:18:45 +0300

    gradientDescentmethod

--- lab2/GradientDescent_method.h
+++ lab2/GradientDescent_method.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <map>
 #include <vector>
 #include <iostream>
@@ -7,42 +6,35 @@
 #include "Parser.h"
 
 struct GradientDescent_method {
-    long double operator()(const std::string &function) {
-        long double eps = 1e-8, alpha = 120;
-        std::map<std::string, long double> x;
-        std::vector<std::string> parsed = Parser(function);
-        x["x"] = 1;
-        x["y"] = -5;
-        long double f = compute_function(function, x);
-        while (true) {
-            std::vector<long double> gradient = compute_gradient(parsed, x);
+	double operator()(const std::string& function) {
+		double eps = 1e-4;
+		double alpha = 1;
+		std::map<std::string, double> x;
+		x["x"] = 0;
+		x["x"] = 0;
+		std::map<std::string, double> newx;
+		int it = 0;
+		while (true) {
+			std::vector<double> gradient = compute_gradient(Parser(function), x);
+			double len = norm(gradient);
 
-            if (norm(gradient) <= eps) {
-                std::map<std::string,long double> min = x;//точка минимума
-                f = compute_function(function, x);//значение в точке минимума
-                return f;
-            }
-            while (true) {
-                std::map<std::string, long double> y;
-                gradient = compute_gradient(parsed, x);
-                int i = 0;
-                for (const auto &p : x) {
-                    std::cout << gradient[i] <<" ";
-                    y[p.first] = x[p.first] - alpha * gradient[i];
-                    i++;
-                }
-                std::cout<<"\n";
-                long double f2 = compute_function(function, y);
-                if (f2 < f) {
-                    x = y;
-                    f = f2;
-                    break;
-                } else {
-                    alpha /= 2;
-                }
+			if (len < eps) {
+				break;
+			}
 
-            }
-        }
-
-    }
+			int i = 0;
+			for (const auto& p : x) {
+				newx[p.first] = x[p.first] - alpha * gradient[i];
+				i++;
+			}
+			double f = compute_function(function, x);
+			double f2 = compute_function(function, newx);
+			if (f < f2) {
+				alpha /= 2;
+			}
+			newx = x;
+			it++;
+		}
+		return compute_function(function, x);
+	}
 };
